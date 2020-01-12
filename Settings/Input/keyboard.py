7a56b7befe2a2117fd53979700202163efaed4e6
Kerimbekov Galym <kegalym2@mail.ru>
2020-01-13 05:07:03 +0600

    Added keymap reading for player actions.

--- Settings/Input/keyboard.py
+++ Settings/Input/keyboard.py
@@ -41,20 +41,40 @@ class Keyboard:
         self.game_settings_filename = base.game_settings_filename
         self.cfg_path = {"game_config_path": "{0}/{1}".format(self.game_cfg_dir, self.game_settings_filename)}
 
-        self.k_left_lo = self.game_settings['Keymap']['left'].lower()
-        self.k_right_lo = self.game_settings['Keymap']['right'].lower()
         self.k_forward_lo = self.game_settings['Keymap']['forward'].lower()
         self.k_backward_lo = self.game_settings['Keymap']['backward'].lower()
+        self.k_left_lo = self.game_settings['Keymap']['left'].lower()
+        self.k_right_lo = self.game_settings['Keymap']['right'].lower()
+        self.k_crouch_lo = self.game_settings['Keymap']['crouch'].lower()
+        self.k_jump_lo = self.game_settings['Keymap']['jump'].lower()
+        self.k_use_lo = self.game_settings['Keymap']['use'].lower()
         self.k_attack_lo = self.game_settings['Keymap']['attack'].lower()
+        self.k_h_attack_lo = self.game_settings['Keymap']['h_attack'].lower()
+        self.k_f_attack_lo = self.game_settings['Keymap']['f_attack'].lower()
+        self.k_block_lo = self.game_settings['Keymap']['block'].lower()
+        self.k_sword_lo = self.game_settings['Keymap']['sword'].lower()
+        self.k_bow_lo = self.game_settings['Keymap']['bow'].lower()
+        self.k_tengri_lo = self.game_settings['Keymap']['tengri'].lower()
+        self.k_umai_lo = self.game_settings['Keymap']['umai'].lower()
 
         self.keymap = {
-            'left': 0,
-            'right': 0,
-            'forward': 0,
-            'backward': 0,
             'cam-left': 0,
             'cam-right': 0,
-            'attack': 0
+            'forward': 0,
+            'backward': 0,
+            'left': 0,
+            'right': 0,
+            'crouch': 0,
+            'jump': 0,
+            'use': 0,
+            'attack': 0,
+            'h_attack': 0,
+            'f_attack': 0,
+            'block': 0,
+            'sword': 0,
+            'bow': 0,
+            'tengri': 0,
+            'umai': 0
         }
 
     # Records the state of the arrow/actions keys
