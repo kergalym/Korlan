639b2c2b02e2f1ae7e60ceb183a979272232e7f9
Kerimbekov Galym <kegalym2@mail.ru>
2020-03-14 02:12:04 +0600

    Shader collection. Restructured.

--- Engine/Render/render.py
+++ Engine/Render/render.py
@@ -1,30 +1,93 @@
+from os.path import exists
+from pathlib import Path, PurePath
+from os import walk
+
 from panda3d.core import *
 
 
 class RenderAttr:
 
     def __init__(self):
+        self.game_dir = str(Path.cwd())
         self.world = None
         self.set_color = 0.2
         self.shadow_size = 1024
-        self.obj = None
         self.render = None
-        self.shadows = Shader.load(Shader.SL_GLSL, vertex="Engine/Shaders/Shadows/vertex.vert",
-                                   fragment="Engine/Shaders/Shadows/fragment.frag")
-        self.ssao = Shader.load(Shader.SL_GLSL, vertex="Engine/Shaders/SSAO/base.vert",
-                                fragment="Engine/Shaders/SSAO/ssao.frag")
+
+    def shader_collector(self):
+        """ Function    : shader_collector
+
+            Description : Collect shader set.
+
+            Input       : None
+
+            Output      : None
+
+            Return      : Dictionary
+        """
+        shader_path = str(PurePath(self.game_dir, "Engine", "Shaders"))
+        shader_path = Filename.from_os_specific("{0}/".format(shader_path))
+        shader_dirs = []
+        shaders = {}
+
+        if exists(shader_path):
+            for root, dirs, files in walk(shader_path, topdown=True):
+                # Get last directory to make it list key
+                d = root.split("/").pop()
+                shader_dirs.append(d)
+
+            for root, dirs, files in walk(shader_path, topdown=True):
+                for d in shader_dirs:
+                    for file in files:
+                        path = str(PurePath("{0}/".format(root), file))
+                        path = Filename.from_os_specific(path).getFullpath()
+                        if d in path:
+                            if "frag" in file:
+                                key = "{0}_{1}".format(d, "frag")
+                                shaders[key] = path
+                            if "vert" in file:
+                                key = "{0}_{1}".format(d, "vert")
+                                shaders[key] = path
+            return shaders
+
+    def get_all_shaders(self, shaders):
+        """ Function    : get_all_shaders
+
+            Description : Get loaded shader set.
+
+            Input       : dict
+
+            Output      : None
+
+            Return      : Dictionary
+        """
+        if shaders and isinstance(shaders, dict):
+            loaded_shaders = {}
+            for k in shaders:
+                # Find shader for vert and frag files
+                # to construct a dict of the loaded shaders
+                name = k.split("_")[0]
+                shader = Shader.load(Shader.SL_GLSL,
+                                     vertex=shaders["{0}_vert".format(name)],
+                                     fragment=shaders["{0}_frag".format(name)])
+                # Contains shader memory addresses
+                loaded_shaders[name] = shader
+            return loaded_shaders
 
     def set_shadows(self, obj, render):
         if obj and render:
-            self.obj = obj
             self.render = render
             # If you don't do this, none of the features
             # listed above will have any effect. Panda will
             # simply ignore normal maps, HDR, and so forth if
             # shader generation is not enabled. It would be reasonable
             # to enable shader generation for the entire game, using this call:
-            self.obj.set_shader_auto()
+            obj.set_shader_auto()
             base.shaderenable = 1
+
+            # TODO Fix me!
+            # ready_shaders = self.get_all_shaders(self.shader_collector())
+            # obj.set_shader(ready_shaders['Shadows'])
 
     def set_lighting(self, name, render, pos, hpr, color, task):
         if (render
@@ -58,4 +121,6 @@ class RenderAttr:
 
     def set_ssao(self, obj):
         if obj:
-            obj.set_shader(self.ssao)
+            # TODO Fix me!
+            ready_shaders = self.get_all_shaders(self.shader_collector())
+            obj.set_shader(ready_shaders['SSAO'])
