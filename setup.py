ea8fe503fea2b09a90dd48728c70b54f712c0173
Kerimbekov Galym <kegalym2@mail.ru>
2020-10-05 15:43:38 +0600

    Refactoring. Stage 17.8. Build version string.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Korlan",
-    version='0.0.1',
+    version='0.1.1',
     include_package_data=True,
     options={
         'build_apps': {
