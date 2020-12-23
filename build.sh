266bc2fcef57f9573deac38d14383430397be94e
Kerimbekov Galym <kegalym2@mail.ru>
2020-12-24 03:25:26 +0600

    Refactoring Stage 26.4. Fix build

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-. venv3*/bin/activate && python3 setup.py build_apps bdist_apps
-cd ~
+  cp requirements_lin.txt requirements.txt
+. venv3*/bin/activate && python3 setup_lin.py build_apps bdist_apps && deactivate
+  cp requirements_win.txt requirements.txt
+. venv3*/bin/activate && python3 setup_win.py build_apps bdist_apps && deactivate
+
