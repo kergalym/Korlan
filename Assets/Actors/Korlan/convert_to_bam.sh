b9a3fd2af6efa382a418282f619ddf9726f84eb4
Kerimbekov Galym <kegalym2@mail.ru>
2022-07-07 00:09:40 +0600

    Refactoring Stage 64.8.  Readd Korlan character

---
+++ Assets/Actors/Korlan/convert_to_bam.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+egg2bam -txo -ctex Korlan_armor.egg -o Korlan_armor.egg.bam 
+egg2bam -txo -ctex Korlan_body.egg -o Korlan_body.egg.bam
+egg2bam -txo -ctex Korlan_boots.egg -o Korlan_boots.egg.bam
+egg2bam -txo -ctex Korlan_cloak.egg -o Korlan_cloak.egg.bam
+egg2bam -txo -ctex Korlan_empty.egg -o Korlan_empty.egg.bam
+egg2bam -txo -ctex Korlan_helmet.egg -o Korlan_helmet.egg.bam
+egg2bam -txo -ctex Korlan_pants.egg -o Korlan_pants.egg.bam
+
+# rm Korlan*.egg
