62f5cc0906c36a3d47f98ab6ed48a37a6da28c49
Kerimbekov Galym <kegalym2@mail.ru>
2022-08-30 04:10:07 +0600

    Refactoring Stage 69.7   Add todos for animation definitions

--- Engine/anim_names.py
+++ Engine/anim_names.py
@@ -13,14 +13,14 @@ a_anim_idle = "Standing_idle_male"
 a_anim_walking = "Walking"
 a_anim_dying = "Dying"
 a_anim_run = "Running"
-a_anim_crouch_idle = "standing_to_crouch"
+a_anim_crouch_idle = "standing_to_crouch"  # todo: replace
 a_anim_crouch_walking = 'crouch_walking_forward'
 a_anim_rolling = "forward_roll"
 # usage
 a_anim_picking = "PickingUp"
 a_anim_dropping = "PickingUp"  # reversed
-a_anim_arm_sword = "sword_disarm_over_shoulder"
-a_anim_arm_bow = "archer_standing_disarm_bow"
+a_anim_arm_sword = "sword_disarm_over_shoulder"  # todo: replace
+a_anim_arm_bow = "archer_standing_disarm_bow"  # todo: replace
 a_anim_disarm_sword = "sword_disarm_over_shoulder"
 a_anim_disarm_bow = "archer_standing_disarm_bow"
 # action
@@ -34,8 +34,8 @@ a_anim_h_attack = "Kicking_3"
 a_anim_f_attack = "Kicking_5"
 a_anim_melee_attack = "great_sword_slash"
 a_anim_archery = "archer_standing_draw_arrow"
-a_anim_get_tengri = ""
-a_anim_get_umai = ""
+a_anim_get_tengri = "PickingUp"   # todo: replace
+a_anim_get_umai = "PickingUp"  # todo: replace
 # social
 a_anim_stand_sit = "standing_to_sit_turkic"
 a_anim_stand_lay = "standing_to_sit_turkic"  # todo: replace
@@ -47,25 +47,25 @@ a_anim_cooking_food = "cook_food"
 a_anim_horse_mounting = "horse_mounting"
 a_anim_horse_unmounting = "horse_unmounting"
 a_anim_horse_rider_idle = "horse_riding_idle"
-a_anim_horse_rider_jumping = ""
-a_anim_horse_rider_crouching = ""
+a_anim_horse_rider_jumping = ""  # todo: add
+a_anim_horse_rider_crouching = ""  # todo: add
 a_anim_horse_rider_arm_sword = "horse_riding_arm_sword"
 a_anim_horse_rider_arm_bow = "horse_riding_arm_bow"
 a_anim_horse_rider_disarm_sword = "horse_riding_arm_sword"
-a_anim_horse_rider_disarm_bow = "horse_riding_arm_bow"
-a_anim_horse_rider_archery = "horse_riding_draw_arrow"
+a_anim_horse_rider_disarm_bow = "horse_riding_arm_bow"  # todo: replace
+a_anim_horse_rider_archery = "horse_riding_draw_arrow"  # todo: replace
 a_anim_horse_rider_swing = "horse_riding_swing"
-a_anim_horse_riger_get_tengri = ""
-a_anim_horse_riger_get_umai = ""
+a_anim_horse_riger_get_tengri = ""  # todo: add
+a_anim_horse_riger_get_umai = ""  # todo: add
 
 """ Horse """
 # movement
 a_anim_horse_idle = "horse_idle"
-a_anim_horse_dying = ""
+a_anim_horse_dying = ""  # todo: add
 a_anim_horse_walking = "horse_walking"
 a_anim_horse_run = "horse_running"
-a_anim_horse_crouch_idle = ""
-a_anim_horse_crouch_walking = ""
+a_anim_horse_crouch_idle = ""  # todo: add
+a_anim_horse_crouch_walking = ""  # todo: add
 # action
-a_anim_horse_standing_crouch = ""
-a_anim_horse_crouching_stand = ""
+a_anim_horse_standing_crouch = ""  # todo: add
+a_anim_horse_crouching_stand = ""  # todo: add
