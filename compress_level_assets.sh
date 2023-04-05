2344a7d52eb18efb1e9480b63fa369b965f163ca
Kerimbekov Galym <kegalym2@mail.ru>
2023-04-06 03:27:13 +0600

    Refactoring Stage 71.8.4. Deploy-related changes

--- compress_level_assets.sh
+++ compress_level_assets.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# egg2bam -o Assets/Levels/lvl_one_rp.egg.bam Assets/Levels/lvl_one_rp.egg
 egg2bam -txo -ctex -o Assets/Levels/lvl_one_rp.egg.bam Assets/Levels/lvl_one_rp.egg
+egg2bam -txo -ctex -o Assets/Levels/lvl_one_grass_rp.egg.bam Assets/Levels/lvl_one_grass_rp.egg
+egg2bam -txo -ctex -o Assets/Levels/lvl_one_trees_rp.egg.bam Assets/Levels/lvl_one_trees_rp.egg
 
