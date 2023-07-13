ccaf3b48a2af2a64b984435335c8a2a31df180f5
Kerimbekov Galym <kegalym2@mail.ru>
2023-07-14 05:39:42 +0600

    Refactoring Stage 72.0.4.

--- Engine/Quests/story_quests.py
+++ Engine/Quests/story_quests.py
@@ -13,33 +13,33 @@ class StoryQuests:
             self.render_pipeline = self.base.game_instance["renderpipeline_np"]
         self._social_quests = social_quests
 
-        self._started_quest_inside_yurt = False
+        self._started_quest_sleeping_inside_yurt = False
 
     def start_story_mode_task(self, task):
         if self.base.game_instance['menu_mode']:
             return task.done
 
         if self.base.game_instance['loading_is_done'] == 1:
-            if not self._started_quest_inside_yurt:
-                self.start_quest_inside_yurt()
+            if not self._started_quest_sleeping_inside_yurt:
+                self.start_quest_sleeping_inside_yurt()
         return task.cont
 
     def init(self):
         self.player = self.base.game_instance["player_ref"]
         self.player_rb_np = self.base.game_instance["player_np"]
 
-    def start_quest_inside_yurt(self):
+    def start_quest_sleeping_inside_yurt(self):
         """ Korlan wakes up inside yurt and see... """
-        self._started_quest_inside_yurt = True
+        self._started_quest_sleeping_inside_yurt = True
 
         yurt_quest_hearth = self.render.find("**/quest_empty_rest_place")
         trigger_np = self.render.find("**/quest_empty_rest_place_trigger")
 
         if yurt_quest_hearth is None:
-            self._started_quest_inside_yurt = False
+            self._started_quest_sleeping_inside_yurt = False
 
         if trigger_np is None:
-            self._started_quest_inside_yurt = False
+            self._started_quest_sleeping_inside_yurt = False
 
         if yurt_quest_hearth is not None:
             self.player_rb_np.set_x(yurt_quest_hearth.get_x())
